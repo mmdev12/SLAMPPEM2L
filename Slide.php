de8fdec7e815a5b93373c7b65d7361e1c5bd0def
Ngarnier <ng161621@edu.ecetech.fr>
2017-02-02 12:21:59 +0100

    Maj erreur jquery

--- Slide.php
+++ Slide.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
 <html>
 <script src="js/jquery.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="css/bootstrap.css">
-<link rel="stylesheet" href="css/bootstrap-theme.min.css">
 <link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="css/bootstrap-theme.css">
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<script src="js/bootstrap.min.js"></script>
 <head>
     <title>Menu</title>
 </head>
@@ -13,84 +11,56 @@
 <body>
 
 
- <div class="container">
- <nav class="navbar navbar-light">
-    <div class="navbar-header">
-    	<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".js-navbar-collapse">
-			<span class="sr-only">Menu</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
-			<a class="navbar-brand" href="index.php">Acceuil</a>
-	</div>
+  <nav class="navbar  navbar-inverse  navbar-fixed-top">
+  <div class="container">
+  <button type="button" class="navbar-toggle"
+  data-toggle="collapse"
+  data-target=".navbar-collapse"
+  >
+  <span class="sr-only"> Toggle navigation</span>
+  <span class="icon-bar"> </span>
+  <span class="icon-bar"> </span>
+  <span class="icon-bar"> </span>
+  </button>
 
-	<div class="collapse navbar-collapse js-navbar-collapse">
-		<ul class="nav navbar-nav">
-            <?php if($page=='livredor'){?>
-                            <li class="active"><?}
-                    elseif($page!='livredor'){?>
-                            <li><?}?>
-                <a href="#">M2L</a></li>
-		</ul>
-        <ul class="nav navbar-nav">
-                <?php if($page=='livredor'){?>
-                                <li class="active"><?}
-                        elseif($page!='livredor'){?>
-                                <li><?}?>
-                    <a href="#">Les Ligues</a></li>
-        </ul>
-        <ul class="nav navbar-nav">
-                <?php if($page=='livredor'){?>
-                                <li class="active"><?}
-                        elseif($page!='livredor'){?>
-                                <li><?}?>
-                    <a href="#">Contact</a></li>
-        </ul>
-                <ul class="nav navbar-nav">
-                <?php if($page=='membres'){?>
-                                <li class="active"><?}
-                        elseif($page!='membres'){?>
-                                <li><?}?>
+   <a class="navbar-brand" href="#"> Bootsnipp</a>
 
-                    <a href="#">Membres</a></li>
-    		</ul>
-            <ul class="nav navbar-nav navbar-right">
-                      <?php if($page=='login'){?>
-                                <li class="active"><?}
-                        if($page!='login'){?>
-                                <li><?}?><a href="login.php">S'inscrire</a></li>
-                      <li class="dropdown">
-                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Se connecter <b class="caret"></b></a>
-                         <ul class="dropdown-menu" style="padding: 15px;min-width: 250px;">
-                            <li>
-                               <div class="row">
-                                  <div class="col-md-12">
-                                     <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-                                        <div class="form-group">
-                                           <label class="sr-only" for="exampleInputEmail2">Email</label>
-                                           <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
-                                        </div>
-                                        <div class="form-group">
-                                           <label class="sr-only" for="exampleInputPassword2">Mot de passe</label>
-                                           <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-                                        </div>
-                                        <div class="checkbox">
-                                           <label>
-                                           <input type="checkbox"> Se souvenir de moi
-                                           </label>
-                                        </div>
-                                        <div class="form-group">
-                                           <button type="submit" class="btn btn-success btn-block">Se connecter</button>
-                                        </div>
-                                     </form>
-                                  </div>
-                               </div>
-                            </li>
-                         </ul>
-                      </li>
-                   </ul>
-    	</div><!-- /.nav-collapse -->
+       <div class="navbar-collapse collapse">
+           <ul class="nav navbar-nav navbar-right">
+		     <li class=""><a href="#">Profile</a> </li>
+			 <li> <a href="#">about</a> </li>
+			    <li class="dropdown">
+			       <a href="#" class="dropdown-toggle"
+				    data-toggle="dropdown"> Learn
+					<b class="caret"> </b>
+					 <ul class="dropdown-menu">
+					   <li class="dropdown-header">Example</li>
+					   <li><a href="#"> c++</a> </li>
+					   <li><a href="#"> java</a> </li>
+					   <li class="dropdown-header">tutorials</li>
+					   <li><a href="#"> c++</a> </li>
+					   <li><a href="#"> java</a> </li>
+					 </ul>
+			   </li>
+		   </ul>
+       </div>
+  </div>
+</nav>
+
+
+  <div class="navbar  navbar-inverse navbar-fixed-bottom">
+     <div class="container">
+       <div class="navbar-text pull=left">
+	    <p> © company 2016.</p>
+	   </div>
+
+	 </div>
+   </div>
+
+
+
+
+</nav>
 
 
   </nav>
--- index.php
+++ index.php
@@ -3,11 +3,9 @@
 
 <!-- BOOTSRAP -->
 <script src="js/jquery.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="css/bootstrap.css">
-<link rel="stylesheet" href="css/bootstrap-theme.min.css">
 <link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="css/bootstrap-theme.css">
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<script src="js/bootstrap.min.js"></script>
 <?php $page='acceuil' ?>
 
 
