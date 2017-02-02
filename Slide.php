b81b22bae51f7b46f69bc093e1ee12e0332ef696
Ngarnier <ng161621@edu.ecetech.fr>
2017-02-02 11:15:37 +0100

    Onglets

--- Slide.php
+++ Slide.php
@@ -20,7 +20,7 @@
 
 <body>
 
- 
+
  <div class="container">
  <nav class="navbar navbar-inverse">
     <div class="navbar-header">
@@ -32,60 +32,74 @@
 		</button>
 			<a class="navbar-brand" href="index.php">Acceuil</a>
 	</div>
-	
+
 	<div class="collapse navbar-collapse js-navbar-collapse">
 		<ul class="nav navbar-nav">
             <?php if($page=='livredor'){?>
                             <li class="active"><?}
                     elseif($page!='livredor'){?>
                             <li><?}?>
-                <a href="livredor.php">Livre d'or</a></li>
+                <a href="#">M2L</a></li>
 		</ul>
-            <ul class="nav navbar-nav">
-            <?php if($page=='membres'){?>
-                            <li class="active"><?}
-                    elseif($page!='membres'){?>
-                            <li><?}?>
-                
-                <a href="membres.php">Membres</a></li>
-		</ul>
-        <ul class="nav navbar-nav navbar-right">
-                  <?php if($page=='login'){?>
-                            <li class="active"><?}
-                    if($page!='login'){?>
-                            <li><?}?><a href="login.php">S'inscrire</a></li>
-                  <li class="dropdown">
-                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">Se connecter <b class="caret"></b></a>
-                     <ul class="dropdown-menu" style="padding: 15px;min-width: 250px;">
-                        <li>
-                           <div class="row">
-                              <div class="col-md-12">
-                                 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-                                    <div class="form-group">
-                                       <label class="sr-only" for="exampleInputEmail2">Email</label>
-                                       <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
-                                    </div>
-                                    <div class="form-group">
-                                       <label class="sr-only" for="exampleInputPassword2">Mot de passe</label>
-                                       <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-                                    </div>
-                                    <div class="checkbox">
-                                       <label>
-                                       <input type="checkbox"> Se souvenir de moi
-                                       </label>
-                                    </div>
-                                    <div class="form-group">
-                                       <button type="submit" class="btn btn-success btn-block">Se connecter</button>
-                                    </div>
-                                 </form>
-                              </div>
-                           </div>
-                        </li>
-                     </ul>
-                  </li>
-               </ul>
-	</div><!-- /.nav-collapse -->
-        
+        <ul class="nav navbar-nav">
+                <?php if($page=='livredor'){?>
+                                <li class="active"><?}
+                        elseif($page!='livredor'){?>
+                                <li><?}?>
+                    <a href="#">Les Ligues</a></li>
+        </ul>
+        <ul class="nav navbar-nav">
+                <?php if($page=='livredor'){?>
+                                <li class="active"><?}
+                        elseif($page!='livredor'){?>
+                                <li><?}?>
+                    <a href="#">Contact</a></li>
+        </ul>
+                <ul class="nav navbar-nav">
+                <?php if($page=='membres'){?>
+                                <li class="active"><?}
+                        elseif($page!='membres'){?>
+                                <li><?}?>
+
+                    <a href="membres.php">Membres</a></li>
+    		</ul>
+            <ul class="nav navbar-nav navbar-right">
+                      <?php if($page=='login'){?>
+                                <li class="active"><?}
+                        if($page!='login'){?>
+                                <li><?}?><a href="login.php">S'inscrire</a></li>
+                      <li class="dropdown">
+                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Se connecter <b class="caret"></b></a>
+                         <ul class="dropdown-menu" style="padding: 15px;min-width: 250px;">
+                            <li>
+                               <div class="row">
+                                  <div class="col-md-12">
+                                     <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                                        <div class="form-group">
+                                           <label class="sr-only" for="exampleInputEmail2">Email</label>
+                                           <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                                        </div>
+                                        <div class="form-group">
+                                           <label class="sr-only" for="exampleInputPassword2">Mot de passe</label>
+                                           <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                        </div>
+                                        <div class="checkbox">
+                                           <label>
+                                           <input type="checkbox"> Se souvenir de moi
+                                           </label>
+                                        </div>
+                                        <div class="form-group">
+                                           <button type="submit" class="btn btn-success btn-block">Se connecter</button>
+                                        </div>
+                                     </form>
+                                  </div>
+                               </div>
+                            </li>
+                         </ul>
+                      </li>
+                   </ul>
+    	</div><!-- /.nav-collapse -->
+
 
   </nav>
 </div>
