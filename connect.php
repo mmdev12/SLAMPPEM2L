8c87f0eca96a97431d06eee6a5694b1600bc1b5c
Ngarnier <ng161621@edu.ecetech.fr>
2017-02-02 11:10:58 +0100

    Connect

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php 
+
+function connectToSQL()
+{
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+	}
+	catch (Exception $e)
+	{
+		die('Erreur : ' . $e->getMessage());
+	}
+
+	return $bdd;
+}
+
+?>
---
+++ login.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+    
+<!-- BOOTSRAP -->
+<script src="js/jquery-3.1.1.js"></script>
+<script src="js/script.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">      
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap-theme.css">
+<?php $page='login' ?>   
+<head>
+    
+    <meta charset="UTF-8">
+    <title>connexion</title>
+    
+</head>
+<body>
+    
+<!-- Menu -->
+<?php include 'slide.php';?>
+<!-- Menu fin -->
+
+<!-- Formulaire nscription -->
+<div class="container">
+
+<div class="row">
+    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+<form action="registration.php" method="POST">
+            <h2>Sign Up</h2>
+        <hr class="colorgraph">
+        <div class="row">
+    		    <div class="col-xs-6 col-sm-6 col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control input-lg" name="nom" id="nom" placeholder="Nom" tabindex="1">
+                    </div>
+                </div>
+    
+                <div class="col-xs-6 col-sm-6 col-md-6">
+                    <div class="form-group">
+                        <input type="text" name="prenom" class="form-control input-lg" placeholder="Prénom" tabindex="2">
+                    </div>
+    		    </div>
+        </div>
+                <div class="form-group">
+                    <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email" tabindex="4">
+                </div>
+        
+    			<div class="row">
+				<div class="col-xs-6 col-sm-6 col-md-6">
+					<div class="form-group">
+						<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Mot de passe" tabindex="5">
+					</div>
+				</div>
+			</div>
+    
+			<hr class="colorgraph">
+			<div class="row">
+                    <form  method="post" action="formpost.php">
+                    <input type="submit" value="S'inscrire" class="btn btn-primary btn-block btn-lg" tabindex="7">
+                    </form>
+			</div>
+    </body>
+</html>
